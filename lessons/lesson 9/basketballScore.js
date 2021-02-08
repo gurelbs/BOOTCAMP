5a5069dae53879f2f1f82fad4bbaae828f288856
gurelbs <guri240@gmail.com>
2021-02-08 18:00:51 +0200

    update lesson 9

--- lessons/lesson 9/basketballScore.js
+++ lessons/lesson 9/basketballScore.js
@@ -1,23 +1,28 @@
-const basketballWinner = () => {
-
-  const basketballScoreCalc = (totalScore,numOfGame) =>  totalScore / numOfGame;
-
-  let JohnTeamScored = basketballScoreCalc(100+20+50,3);
-  let MikeTeamScored = basketballScoreCalc(100+50+20,3);
-  let MaryTeamScored = basketballScoreCalc(197+94+105,3);
-
-  const bigestScore = (a,b,c) => Math.max(a,b,c);
-  const winner = bigestScore(JohnTeamScored,MikeTeamScored,MaryTeamScored);
-  let winnerMsg = () => {
-    if (JohnTeamScored === winner){
-      return `John's team WIN with ${JohnTeamScored.toFixed()} average score.`
-    } else if (MikeTeamScored === winner){
-      return `Mike's team WIN with ${MikeTeamScored.toFixed()} average score.`
-    } else if (MaryTeamScored === winner) {
-      return `Mary's team WIN with ${MaryTeamScored.toFixed()} average score.`
+const basketballScoreCalc = (totalScore,numOfGame) =>  (totalScore / numOfGame).toFixed(1);
+let JohnTeamScored = basketballScoreCalc(102+200+100,3);
+let MikeTeamScored = basketballScoreCalc(101+201+100,3);
+let MaryTeamScored = basketballScoreCalc(101+90+101,3);
+let bigestScore = Math.max(JohnTeamScored,MikeTeamScored,MaryTeamScored);
+  
+let answer = () => {
+  if (JohnTeamScored > MikeTeamScored && JohnTeamScored > MaryTeamScored){
+    return `John's Team WIN with ${bigestScore}`
+  } else if (MikeTeamScored > JohnTeamScored && MikeTeamScored > MaryTeamScored){
+    return `Mike's Team WIN with ${bigestScore}`
+  } else if (MaryTeamScored > JohnTeamScored && MaryTeamScored > MikeTeamScored){
+    return `Mary's Team WIN with ${bigestScore}`
+  } else if (MikeTeamScored === MaryTeamScored && MikeTeamScored === JohnTeamScored){
+    return `its a drew between EVERY team`
+  } else {
+    if (JohnTeamScored === MikeTeamScored){
+      return `its a drew between John's Team & Mike's Team`
+    }
+    if (JohnTeamScored === MaryTeamScored){
+      return `its a drew between John's Team & Mary's Team`
+    }
+    if (MikeTeamScored === MaryTeamScored){
+      return `its a drew between Mike's Team & Mary's Team`
     }
   }
-  return winnerMsg() 
 }
-
-console.log(basketballWinner());
+console.log(answer());
--- lessons/lesson 9/boom.js
+++ lessons/lesson 9/boom.js
@@ -1,6 +1,8 @@
 let boom = n => {
     for (let i = 1; i <= n; i++){
-        if (i % 7 === 0 && i !== 0 || i.toString().includes(7)){
+        if (i % 7 === 0 && i.toString().includes(7)){
+            console.log(`boom-boom!`.toUpperCase());
+        } else if(i % 7 === 0){
             console.log(`boom!`.toUpperCase());
         } else {
             console.log(i);
