4b638624a9b332d6dab987299cb2f54d6029e3a8
gurelbs <guri240@gmail.com>
2021-03-22 23:57:58 +0200

    update

--- lessons/lesson-30 (week-9)/13.1-data_massaging/src/App.js
+++ lessons/lesson-30 (week-9)/13.1-data_massaging/src/App.js
@@ -1,39 +1,75 @@
 import React, { Component } from 'react';
 import Data from './Data'
+import Name from './Name'
+import Card from './Card'
+import './App.css'
 export default class App extends Component{
-    constructor(props){
-        super(props)
-        this.state = {
-            data: Data,
-        }
+    state = { 
+        data: Data,
+        burnBefore1990 : [],
+        burnAfter1990 : [],
     }
-    allTheNames = () => {
-        let {data} = this.state
-        let names = []
+    componentDidMount(){
+        let { data } = this.state
+        this.setState({
+            ...this.state,
+            burnBefore1990: data.filter(person => new Date(person.birthday).getFullYear() < 1990),
+            burnAfter1990: data.filter(person => new Date(person.birthday).getFullYear() >= 1990)
+        })
+    }
+    allTheNames = () => 
+        this.state.data.map(( (person,i) => 
+        <Name key={i} name={person.name}/>))
 
-        data.map((person => names.push(person.name)))
-        return (
-            <ul>names: {names.map(((name,i) => <li key={i}>{name}</li>))}</ul>
+    burnBefore1990 = () => {
+        let {burnBefore1990} = this.state
+        return burnBefore1990.map((person,i) => 
+        <li key={i}>
+            {Object.values(person).slice(0,2).join(' ---> ')}
+        </li>)
+    }
+    burnAfter1990 = () => {
+        let {burnAfter1990} = this.state
+        return burnAfter1990.map((person,i) => 
+        <li key={i}> 
+            {Object.values(person).slice(0,2).join(' ---> ')}
+        </li>)
+    }
+    renderCardDetails = () => {
+        return this.state.burnBefore1990.map((person,i) => 
+            <Card
+                key={i}
+                name={person.name}
+                birthday={person.birthday}
+                favoriteFish={person.favoriteFoods.fish.join(', ')}
+                favoriteMeats={person.favoriteFoods.meats.join(', ')}
+            />
             )
         }
-        burnBefore1990 = () => {
-            let {data} = this.state
-            let newData = data
-                .filter(person => new Date(person.birthday).getFullYear() < 1990) 
-                .map((obj,i ) => <li key={i}>{`${obj.name} ${obj.birthday}`}</li>)
-                this.setState(state => {state.newData: newData})
-            return (
-                <ul>
-                    {newData}
-                </ul>
-            )
-            
-    }
     render(){
         return (
             <div>
-                {this.allTheNames()}
-                {this.burnBefore1990()}
+                <h1>all data: </h1>
+                {JSON.stringify(this.state.data)}
+                <div>
+                    <div>
+                        <h2>allTheNames</h2>
+                        <ul>{this.allTheNames()}</ul>
+                    </div>    
+                    <div>
+                        <h2>burnBefore1990</h2>
+                        <ul>
+                            {this.burnBefore1990()}
+                        </ul>
+                    </div>    
+                    <div>
+                        <h2>burnAfter1990</h2>
+                        <ul>
+                            {this.burnAfter1990()}
+                        </ul>
+                    </div>
+                    <div className='card'>{this.renderCardDetails()}</div> 
+                </div>
             </div>
         );
     }
---
+++ lessons/lesson-30 (week-9)/13.1-data_massaging/src/Card.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Card = ({name,birthday,favoriteFish,favoriteMeats}) => {
+    return (
+        <div>
+            <h1>name: {name}</h1>
+            <h4>birthday: {birthday}</h4>
+            favoriteFoods: 
+            <ul>
+                favoriteFish:
+                {favoriteFish}
+            </ul>
+            <ul>
+                favoriteMeats:
+                {favoriteMeats}
+            </ul>
+        </div>
+    );
+}
+
+export default Card;
