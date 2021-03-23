92074520a12443b6633a18b247c2e263266141eb
gurelbs <guri240@gmail.com>
2021-03-23 23:39:32 +0200

    update

--- lessons/lesson-30 (week-9)/14.2-copy/src/App.js
+++ lessons/lesson-30 (week-9)/14.2-copy/src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(props){
+    super(props)
+    this.state= {
+      p: ''
+    }
+    this.txtAreaRef = React.createRef()
+  }
+  copy = () => {
+    this.textArea.select()
+    document.execCommand('copy')
+  }
+  paste = () => this.setState({p: this.textArea.value})
+  render(){
+    return (
+      <div className="App">
+        <h1>type anything you want!</h1>
+        <textarea ref={textarea => this.textArea = textarea}/>
+        <div>
+          <button onClick={this.copy}>Copy</button>
+        </div>
+        <p ref={p => this.paraGraph = p}>{this.state.p || null}</p>
+        <button onClick={this.paste} >paste</button>
+      </div>
+    );
+  }
 }
-
-export default App;
