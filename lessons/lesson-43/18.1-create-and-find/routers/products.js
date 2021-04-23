d8aae2e0d645c4e04ee10d9ef4322b5e7215be4c
gurelbs <guri240@gmail.com>
2021-04-23 06:34:46 +0300

    update delete and update cmd

--- lessons/lesson-43/18.1-create-and-find/routers/products.js
+++ lessons/lesson-43/18.1-create-and-find/routers/products.js
@@ -60,5 +60,40 @@ router.get('/api/products/:id', async (req,res) => {
         res.status(500).send('product not founded\n' + e.message) 
     }
 })
+router.patch('/api/products/:id', async (req,res) => {
+    const updated = Object.keys(req.body)
+    const allowed = ["isActive"]
+    const valid = updated.every( up => allowed.includes(up))
+    if (!valid) res.status(400).send('invalid update feild..')
+    try {
+        const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        })
+        if (!product) res.status(404).send('product not found')
+        res.status(201).json(product)
+    } catch (e) {
+        res.status(500).send('product not founded\n' + e.message) 
+    }
+})
 
+router.delete('/api/products/:id', async (req, res) => {
+    const product = await Product.findByIdAndDelete(req.params.id)
+    const count = await Product.count()
+    try {
+        if (!product) res.status(404).json('product not found')
+        res.status(200).send('products deleted successfully\n'+`products left: ${count}`)
+    } catch (e) {
+        res.status(500).send('there is some error\n' + e.message) 
+    }
+})
+router.delete('/api/products', async (req, res) => {
+    const products = await Product.deleteMany({})
+    if (!products) res.status('products not found')
+    try {
+        res.status(200).send('all products deleted successfully')
+    } catch (e) {
+        res.status(500).send('there is some error\n' + e.message) 
+    }
+})
 module.exports = router
